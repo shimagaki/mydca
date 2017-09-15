21b037f340956ad3c179e09a37049aea72ed6a76
shimagaki <kai0shimagaki@gmail.com>
2017-09-15 11:19:40 +0200

    Added c++ code

---
+++ compleat/c++/gen_training_data.cpp
@@ -0,0 +1,122 @@
+// Generative model of potts. 
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+int q=2, L=16;
+double J0 = 1.0;
+
+/*****************************/
+vector<int> X(L,0);
+vector < vector<double> > J( L*L, vector<double>(q*q,0) );  
+/*****************************/
+int T_equil = 1e5;
+int T_interval = 50;
+int N_sampling = 1e4;
+/*****************************/
+inline double std_rand()
+{
+    return rand() / (RAND_MAX + 1.0);
+}
+
+void J_generalized_potts(){
+	double r;
+	for(int i=0;i<L;i++){
+	for(int j=i+1;j<L;j++){
+	//for(int j=0;j<L;j++){
+		for(int a=0;a<q;++a){
+		for(int b=0;b<q;++b){
+			r = std_rand();
+		//	J[i*L+j][a*q+b] = count;
+			J[i*L+j][a*q+b] = r;
+			J[j*L+i][b*q+a] = r;
+		}}}}
+	string fname = "J_data_L"+to_string(L)+"_c++.dat";
+	ofstream file(fname);
+	for(int i=0;i<L;i++){
+	for(int j=0;j<L;j++){
+		for(int a=0;a<q;++a){
+		for(int b=0;b<q;++b){
+			file << J[i*L+j][a*q+b] << endl; 
+		}}}}
+	file.close();
+}
+double E_local(int i){
+	int a = X[i],b;
+	double E_i = 0.0;
+	for(int j=0;j<L;++j){
+		b = X[j];
+		E_i += -J[i*L+j][a*q+b];	
+	}
+	return E_i;	
+}
+
+double E(){
+	double E_tot = 0.0;
+	for(int i=0;i<L;++i){
+		E_tot = E_local(i);
+	}
+	return E_tot / 2.0;
+}
+
+bool Metropolis(int i){
+	double E_i, E_i_trial, dE, w;
+	int Xi_save;
+	bool accepted;
+	
+	E_i= E_local(i);
+	Xi_save = X[i];
+	X[i] = (X[i]+int(std_rand()*q+1)) % q;
+	E_i_trial = E_local(i);
+	dE = E_i_trial - E_i;
+	w =exp(-dE);
+       	accepted = false;
+	if(w > std_rand())
+       		accepted = true;
+	else
+		X[i] = Xi_save;
+	return accepted;
+}
+
+int MonteCarlo_sweep(){
+	double accepted;
+	int i,l;
+	for(l=0;l<L;++l){
+		i = int(L*std_rand());
+		if(Metropolis(i))
+			accepted += 1;
+	}
+	return accepted;
+}
+
+int main(){
+	J_generalized_potts();
+	string fname = "test_training_data_L" + to_string(L) + "_c++.dat";
+	ofstream file(fname);
+	int t;
+	t = 0;
+	while(t<T_equil){
+		MonteCarlo_sweep();	
+		t+=1;
+	}
+	t = 0;
+	while(t<T_interval*N_sampling){
+		MonteCarlo_sweep();
+		if(t%T_interval == 0){
+		
+		for(int i=0;i<L;++i){
+			file << X[i] << " ";	
+		}
+			file << endl;
+	}
+		t+=1;
+	}
+	file.close();
+}
+
+
---
+++ compleat/c++/inverse_ml_basic.cpp
@@ -0,0 +1,247 @@
+// Maximamu Likelihood estimation for inverse potts model. 
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+int q=2, L=16;
+double J0 = 1.0;
+
+/*****************************/
+vector<int> X(L,0);
+vector < vector<double> > J( L*L, vector<double>(q*q,0) );  
+vector < vector<double> > J_data( L*L, vector<double>(q*q,0) );  
+vector < vector<double> > Psi( L*L, vector<double>(q*q,0) );  
+vector < vector<double> > Psi_model( L*L, vector<double>(q*q,0) );  
+
+/*****************************/
+int T_epoch = 100;
+int T_equil = 100;
+int T_interval = 1e2;
+///int N_sample = 1e4;
+int N_sample = 1800;
+int M = 100 ; // number of sample of average by statistical model. 
+double lr = 0.05;
+double eps = 0.0;
+
+/*****************************/
+inline double std_rand()
+{
+    return rand() / (RAND_MAX + 1.0);
+}
+
+double E_local(int i){
+	int a = X[i],b;
+	double E_i = 0.0;
+	for(int j=0;j<L;++j){
+		b = X[j];
+		E_i += -J[i*L+j][a*q+b];	
+	}
+	return E_i;	
+}
+
+double E(){
+	double E_tot = 0.0;
+	for(int i=0;i<L;++i){
+		E_tot = E_local(i);
+	}
+	return E_tot / 2.0;
+}
+
+bool Metropolis(int i){
+	double E_i, E_i_trial, dE, w;
+	int Xi_save;
+	bool accepted;
+	
+	E_i= E_local(i);
+	Xi_save = X[i];
+	X[i] = (X[i]+int(std_rand()*q+1)) % q;
+	E_i_trial = E_local(i);
+	dE = E_i_trial - E_i;
+	w =exp(-dE);
+       	accepted = false;
+	if(w > std_rand())
+       		accepted = true;
+	else
+		X[i] = Xi_save;
+	return accepted;
+}
+
+int MonteCarlo_sweep(){
+	double accepted;
+	int i,l;
+	for(l=0;l<L;++l){
+		i = int(L*std_rand());
+		if(Metropolis(i))
+			accepted += 1;
+	}
+	return accepted;
+}
+
+bool J_data_init(){
+	bool flag=true;
+	string fname_in = "J_data_L"+to_string(L)+"_c++.dat";
+  	ifstream file(fname_in);
+  	string line;
+  	int count = 0; int i=0,j=0,a,b,var; // this initialize is necessary.
+  	if (file.is_open()){
+    	while ( getline (file,line,'\n'))
+    	{
+		var = count;
+	    	b = var % q;
+		var = var - b;
+		a = (var/q)%q;
+		var = (var - a*q)/(q*q);
+		j = var%L;	
+		i = int(var/L);
+		J_data[L*i+j][a*q+b] = stod(line);
+		count += 1;
+		/*
+		cout << J_data[L*i+j][a*q+b] << " ";
+		if(count%L == 0)
+			cout << endl;
+		*/
+    	}
+    	file.close();
+	flag = true;
+	}
+  	else{ cout << "Unable to open file" << endl;  
+  	cout << fname_in << endl;
+    	flag = false;
+  	}
+	return flag;
+}
+
+bool average_data(){
+	bool flag=true;
+	string fname_in = "test_training_data_L"+to_string(L)+"_c++.dat";
+  	ifstream file(fname_in);
+  	string line;
+  	int count = 0; 
+	int i=0,j=0,a,b;
+	if (file.is_open())
+	{
+		count = 0;
+		while (getline (file,line,' ') && count<(N_sample*L) ) 
+		{
+			X[count%L] = stoi(line);
+			if(count%L==0 && count > 0){
+				for(int i=0;i<L;++i){
+				for(int j=i+1;j<L;++j){
+					a = X[i]; b = X[j];
+					Psi[i*L+j][a*q+b] += 1.0;
+					Psi[j*L+i][b*q+a] += 1.0;
+		}}}
+			count += 1;
+		}
+		for(i=0;i<L;++i){
+		for(j=i+1;j<L;++j){
+			for(int a=0;a<q;++a){
+			for(int b=0;b<q;++b){
+				Psi[i*L+j][a*q+b] /= N_sample;
+				Psi[j*L+i][b*q+a] /= N_sample;
+			}}}}
+		file.close();
+		flag = true;
+	}
+  	else{ cout << "Unable to open file" << endl;  
+  	cout << fname_in << endl;
+    	flag = false;
+  	}
+	return flag;
+}
+
+void Psi_model_init(){
+	for(int i=0;i<L;++i){
+	for(int j=0;j<L;++j){
+		for(int a=0;a<q;++a){
+		for(int b=0;b<q;++b){
+			Psi_model[i*L+j][a*q+b] = 0.0;
+			Psi_model[j*L+i][b*q+a] = 0.0;
+	}}}}	
+}
+
+void X_init(){
+	for(int i=0;i<L;++i){
+		X[i] = int(q*std_rand());
+	}
+}
+
+int  average_model(){
+	Psi_model_init();
+	X_init();
+	int t,i,j,a,b,accepted=0;
+	for(t=0;t<T_equil;++t){
+		MonteCarlo_sweep();
+	}
+
+	for(t=0;t<T_interval*M;++t){
+		accepted += MonteCarlo_sweep();
+		if(t%T_interval == 0){
+			for(i=0;i<L;++i){
+			for(j=i+1;j<L;++j){
+				a = X[i]; b = X[j];
+				    Psi_model[i*L+j][a*q+b] += 1.0/M; 
+				    Psi_model[j*L+i][b*q+a] += 1.0/M;
+	}}}}
+	return accepted ;
+}
+
+double gradient_descent(){
+	double error = 0.0,dPsi;
+	int i,j,a,b;
+	for(i=0;i<L;++i){
+	for(j=i+1;j<L;++j){
+		for(a=0;a<q;++a){
+		for(b=0;b<q;++b){
+			//cout << Psi[i*L+j][a*q+b] << "  " << Psi_model[i*L+j][a*q+b] << endl;
+			dPsi = (Psi[i*L+j][a*q+b] -  Psi_model[i*L+j][a*q+b]);
+			error += dPsi*dPsi ;
+			J[i*L+j][a*q+b] +=  lr * (dPsi - eps*J[i*L+j][a*q+b]); 
+			
+			dPsi = (Psi[j*L+i][b*q+a] - Psi_model[j*L+i][b*q+a]);
+			error += dPsi*dPsi ;
+			J[j*L+i][b*q+a] +=  lr * (dPsi - eps*J[j*L+i][b*q+a]); 
+		}}	
+	}}
+    error = sqrt(error) / (L*L*q*q);
+	return error;
+}
+
+void outputestimator(){
+	//string fname = "J_model_estimator_L"+to_string(L)+"_c++.dat";
+	string fname = "J_model_estimator_L"+to_string(L)+"_c++.dat";
+	ofstream file(fname);
+	for(int i=0;i<L;i++){
+	for(int j=0;j<L;j++){
+		for(int a=0;a<q;++a){
+		for(int b=0;b<q;++b){
+			file << J[i*L+j][a*q+b] << endl; 
+		}}}}
+	file.close();
+}
+
+int main(){
+	double error,time_start,time_end,dtime;
+	int t;
+	bool flag;
+
+	flag = J_data_init();
+	average_data();
+	time_start = time(0);
+	for(t=0;t<T_epoch;++t){
+		average_model();
+		error = gradient_descent();
+		cout << t <<"  " << error << endl;
+	}
+	time_end = time(0); 
+	dtime = time_end - time_start;
+	cout << "#ML: computational time = " << dtime <<endl;
+	outputestimator();
+	
+	
+}
